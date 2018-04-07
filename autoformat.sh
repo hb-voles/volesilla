76f832d28bf4335aec62f2cc7f37e8d9a8f0c4f1
celestian <petr.celestian@gmail.com>
2018-04-07 10:37:32 +0200

    Pylint

---
+++ autoformat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Obtain the path to the work directory
+RELATIVE_SOURCE_PATH=`dirname ${BASH_SOURCE[@]}`
+SOURCE_PATH=`readlink -f ${RELATIVE_SOURCE_PATH}`
+
+# Removing of trailing spaces
+sed --in-place 's/[[:space:]]\+$//' `find ${SOURCE_PATH} -type f -name '*.md'`
+
+# Reformat all markdown files to markdown_github
+for filename in `find ${SOURCE_PATH} -type f -name '*.md'`; do
+
+    pandoc \
+        -s \
+        --smart \
+        -f markdown_github \
+        -t markdown_github \
+        -o ${filename}.tmp \
+        ${filename}
+
+    mv -f ${filename}.tmp ${filename}
+
+done
+
+autopep8 --max-line-length=100 -i -aaa ${SOURCE_PATH}/volesilla.py
+
