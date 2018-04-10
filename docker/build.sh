67a33ef4f930e0c0244253df46c976a6f2c1b96d
celestian <petr.celestian@gmail.com>
2018-04-10 18:33:52 +0200

    Fix: variable exports 2

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
 tox -r
-
-export FLASK_DEBUG=0
-
-export VOLESILLA_COMMIT_HASH=`git log -1 --pretty=format:%h`
-export VOLESILLA_COMMIT_TS=`git log -1 --pretty=format:%ct`
-export VOLESILLA_DEPLOY_TS=`date +%FT%T%Z`
-
-echo $VOLESILLA_COMMIT_HASH
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 source .tox/py36/bin/activate
+
+export FLASK_DEBUG=0
+export VOLESILLA_COMMIT_HASH=`git log -1 --pretty=format:%h`
+export VOLESILLA_COMMIT_TS=`git log -1 --pretty=format:%ct`
+export VOLESILLA_DEPLOY_TS=`date +%FT%T%Z`
+
 FLASK_APP=autoapp.py flask run --host=0.0.0.0 --port=80
