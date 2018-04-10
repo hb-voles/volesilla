97f38215c2fddc3ffaf40dd3e4385969b1c13a7c
celestian <petr.celestian@gmail.com>
2018-04-10 17:56:59 +0200

    Better layout

--- app/about/views.py
+++ app/about/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """User views."""
+from flask import current_app as app
 from flask import Blueprint, render_template
 from jinja2 import TemplateNotFound
 
@@ -9,4 +10,13 @@ blueprint = Blueprint('about', __name__, template_folder='templates')
 
 @blueprint.route('/')
 def index():
-    return render_template('pages/about.html')
+
+    commit_hash = app.config['COMMIT_HASH']
+    commit_ts = app.config['COMMIT_TS']
+    deploy_ts = app.config['DEPLOY_TS']
+
+    return render_template(
+        'pages/about.html',
+        commit_hash=commit_hash,
+        commit_ts=commit_ts,
+        deploy_ts=deploy_ts)
