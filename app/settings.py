23d8feeed54ac32c350586e45ff39498732fb26a
celestian <petr.celestian@gmail.com>
2018-04-11 17:55:00 +0200

    Add mail support

--- app/settings.py
+++ app/settings.py
@@ -16,6 +16,17 @@ class Config(object):
     COMMIT_HASH = os.environ.get('VOLESILLA_COMMIT_HASH', 'commit-hash')
     DEPLOY_TS = os.environ.get('VOLESILLA_DEPLOY_TS', 'deploy-ts')
 
+    # Flask-Mail settings
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', '@example.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'password')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', '"MyApp" <noreply@example.com>')
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', '465'))
+    MAIL_USE_SSL = int(os.getenv('MAIL_USE_SSL', True))
+
+    # Flask-User settings
+    USER_APP_NAME = "volesilla"                # Used by email templates
+
 
 class ProdConfig(Config):
     """Production configuration."""
