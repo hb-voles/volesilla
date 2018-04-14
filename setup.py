604bc231f95c2d144a7051fda88806fd78c3517d
celestian <petr.celestian@gmail.com>
2018-04-14 08:09:39 +0200

    More exact versions of dependecies

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ setup(
 
     install_requires=[
         'Flask==0.12',
-        'flask-bootstrap',
+        'Flask-Bootstrap==3.3.7.1',
         'Flask-User==0.6.19',
-        'Flask-Mail',
-        'Flask-Babel',
-        'Flask-BabelEx',
-        'Flask-WTF',
+        'Flask-Mail==0.9.1',
+        'Flask-Babel==0.11.2',
+        'Flask-BabelEx==0.9.3',
+        'Flask-WTF==0.14.2',
         'Flask-Nav',
         'docopt',
-        'uwsgi',
+        'uWSGI==2.0.17',
         'pylint',
     ],
     setup_requires=[
