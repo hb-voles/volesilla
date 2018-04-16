e06ee6f9f6f812519118a2c84007b1826efa7344
celestian <petr.celestian@gmail.com>
2018-04-16 07:01:24 +0200

    Fix on entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 source .tox/py36/bin/activate
-
-source .deploy_env.sh
-export FLASK_DEBUG=0
-
 python volesilla_utils.py db_init volesilla.db
-
-uwsgi --http 0.0.0.0:80 --wsgi-file volesilla.py --callable app --master --processes 5 --pidfile /tmp/volesilla.pid
+uwsgi \
+    --http 0.0.0.0:80 \
+    --wsgi-file volesilla.py \
+    --callable app \
+    --master \
+    --processes 5 \
+    --pidfile /tmp/volesilla.pid
