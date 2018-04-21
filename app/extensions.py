1027b443bf0d8a50463a4d6d062b33ad62c673a7
celestian <petr.celestian@gmail.com>
2018-04-21 15:29:44 +0200

    Simple login
    label: feature
    
    Resolves: #3

--- app/extensions.py
+++ app/extensions.py
@@ -1,42 +1,14 @@
 # -*- coding: utf-8 -*-
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import UserMixin, SQLAlchemyAdapter
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from flask_nav import Nav
+from flask_wtf.csrf import CSRFProtect
 
-
+flask_bcrypt = Bcrypt()  # pylint: disable=invalid-name
 db = SQLAlchemy()  # pylint: disable=invalid-name
-
-
-class User(db.Model, UserMixin):
-    '''User data model'''
-    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=invalid-name
-
-    # User authentication information
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False, server_default='')
-
-    # User email information
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    confirmed_at = db.Column(db.DateTime())
-
-    # User information
-    active = db.Column('is_active', db.Boolean(), nullable=False,
-                       server_default='0')
-    first_name = db.Column(db.String(100), nullable=False, server_default='')
-    last_name = db.Column(db.String(100), nullable=False, server_default='')
-
-
-class Internal(db.Model):
-    '''Table for internal data'''
-    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=invalid-name
-    db_version = db.Column(db.Integer)
-
-
-db_adapter = SQLAlchemyAdapter(db, User, Internal)  # pylint: disable=invalid-name
 mail = Mail()  # pylint: disable=invalid-name
 bootstrap = Bootstrap()  # pylint: disable=invalid-name
-nav = Nav()  # pylint: disable=invalid-name
+csrf = CSRFProtect()  # pylint: disable=invalid-name
--- volesilla_utils.py
+++ volesilla_utils.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """volesilla_utils
 Usage:
-  volesilla_utils.py db_check <db_file>
   volesilla_utils.py db_init <db_file>
   volesilla_utils.py (-h | --help)
 Options:
@@ -11,49 +10,22 @@ Options:
 
 import os
 import sys
+from datetime import datetime
 from docopt import docopt
 from flask.helpers import get_debug_flag
 
 from app.app import create_app
 from app.settings import DevConfig, ProdConfig
-from app.extensions import db, db_adapter, Internal
+from app.extensions import db
+
+from app.extensions import flask_bcrypt
+from app.account.model import User
 
 
 def main():
     """Entry point"""
 
     args = docopt(__doc__)
-
-    if args['db_check'] and args['<db_file>']:
-
-        config = DevConfig if get_debug_flag() else ProdConfig
-
-        db_dir = os.path.join(config.PROJECT_ROOT, 'data')
-        config.DB_NAME = args['<db_file>']
-        config.DB_PATH = os.path.join(db_dir, config.DB_NAME)
-        config.SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(config.DB_PATH)
-
-        if not os.path.isfile(config.DB_PATH):
-            print('[ERROR] File [{}] doesn\'t exist!'.format(config.DB_PATH))
-            sys.exit(2)
-
-        app = create_app(config_object=config)
-        db_version_from_conf = app.config['DB_VERSION']
-
-        with app.app_context():
-            db.init_app(app)
-            db_version_from_file = db_adapter.get_object(Internal, 1).db_version
-
-        if db_version_from_conf == db_version_from_file:
-            print(
-                '[SUCCESS] File [{}] has the same version as is needed. [version: {}]'.format(
-                    config.DB_PATH,
-                    db_version_from_conf))
-            sys.exit(0)
-        else:
-            print('[WARNING] File [{}] has different version [{}] as is needed [{}].'.format(
-                config.DB_PATH, db_version_from_file, db_version_from_conf))
-            sys.exit(1)
 
     if args['db_init'] and args['<db_file>']:
 
@@ -77,9 +49,14 @@ def main():
             db.init_app(app)
             db.create_all()
 
-            internal = Internal(db_version=config.DB_VERSION)
+            admin = User(
+                username='admin',
+                password=flask_bcrypt.generate_password_hash(config.APP_ADMIN_PASS),
+                email=config.APP_ADMIN_MAIL,
+                confirmed_at=datetime.now(),
+                active=True)
 
-            db.session.add(internal)
+            db.session.add(admin)
             db.session.commit()
 
         print('[SUCCESS] File [{}] created.'.format(config.DB_PATH))
