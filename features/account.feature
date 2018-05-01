8450a30f2142e740bb90990f8ab95edaf2d307d6
celestian <petr.celestian@gmail.com>
2018-05-01 07:18:33 +0200

    Test on reset password

--- features/account.feature
+++ features/account.feature
@@ -15,3 +15,14 @@ Feature: Account
     And we can see "Given e-mail isn&#39;t registered!" on loaded page
     And there is no active "reset-password" token for user "admin@test.vls"
     And no mail was sent
+
+  Scenario: Reset password
+    Given we have vls running with admin user "admin@test.vls"
+    and we have "reset-password" token for user "admin@test.vls"
+    and confirmed_at is set old for user "admin@test.vls"
+    When we reset password to "Password123"
+    Then return status is "200"
+    And we can see "Successfully Password Changed" on loaded page
+    And there is no active "reset-password" token for user "admin@test.vls"
+    And confirmed_at is updated for user "admin@test.vls"
+    And password of user "admin@test.vls" is "Password123"
--- features/steps/server.py
+++ features/steps/server.py
@@ -2,13 +2,17 @@
 
 import os
 from multiprocessing import Process
+from bs4 import BeautifulSoup
 
 from app.app import create_app
 from app.settings import TestConfig
+from app.extensions import MAIL
+
+from database import init_db, check_db_created, add_user, check_user_added
 
 
 def start_server(test_dir, db_file):  # pylint: disable=unused-argument
-    """Create client"""
+    """Run server"""
 
     vls = {}
 
@@ -38,3 +42,46 @@ def start_server(test_dir, db_file):  # pylint: disable=unused-argument
 
     # with context.session.session_transaction() as sess:
     #     sess['key here'] = some_value
+
+
+def start_server_with_admin(test_dir, user_mail):
+    """Run vls server with admin user"""
+
+    proc_init, db_file = init_db(test_dir)
+    check_db_created(proc_init, db_file)
+
+    proc_add_user = add_user(db_file, user_mail)
+    check_user_added(proc_add_user, db_file, user_mail)
+
+    return start_server(test_dir, db_file)
+
+
+def ask_for_reset_password_token(client, user_mail):
+    """Ask for password reset"""
+
+    response = client.get('/password/forgotten')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    csrf = soup.find('input', {'id': 'csrf_token'})
+
+    with MAIL.record_messages() as outbox:
+
+        response = client.post('/password/forgotten', data=dict(
+            csrf_token=csrf['value'],
+            email=user_mail,
+        ), follow_redirects=True)
+
+    return response, outbox
+
+
+def reset_password(client, token, password):
+    """Ask for password reset"""
+
+    response = client.get('/password/reset/{}'.format(token))
+    soup = BeautifulSoup(response.data, 'html.parser')
+    csrf = soup.find('input', {'id': 'csrf_token'})
+
+    return client.post('/password/reset/{}'.format(token), data=dict(
+        csrf_token=csrf['value'],
+        password1=password,
+        password2=password
+    ), follow_redirects=True)
